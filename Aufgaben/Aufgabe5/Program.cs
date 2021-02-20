6cac7b8baddd0ec44d87ced90b30e2397ebe1c14
bennimoser <62222316+bennimoser@users.noreply.github.com>
2021-02-20 15:32:45 +0100

    Aufgabe 7 started & Aufgabe 5 finished

---
+++ Aufgaben/Aufgabe5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var processes = Process.GetProcesses();
+
+            //Important! Buildoptions: 32 bit bevorzugen entwählen!!!
+            //Get Modules
+            List<ProcessModuleCollection> list = new List<ProcessModuleCollection>();
+            foreach(var process in processes)
+            {
+                try
+                {
+                    var modules = process.Modules;
+                    list.Add(modules);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+
+            //Check on .Net Assembly
+            foreach (var modules in list)
+            {
+                for (int i = 0; i < modules.Count; i++)
+                {
+                    var module = modules[i];
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFrom(module.FileName);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(module.ModuleName + "is an .Net Assembly");
+                    }
+                    catch(BadImageFormatException e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(module.FileName + "is not an .Net Assembly");
+                    }
+                    catch(FileNotFoundException e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(module.FileName + "! File not found!");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Aufgaben/Aufgabe7/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe7
+{
+    public static class Extension
+    {
+        public static IEnumerable<T> Apply<T>(this IEnumerable<T> objects, Func<T, T> function)
+        {
+            foreach(var item in objects){
+                yield return function.Invoke(item);
+            }
+        }
+    }
+}
