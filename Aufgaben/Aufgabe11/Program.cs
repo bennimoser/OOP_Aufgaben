941eaea5237136eb86549be2cc47e008831d273e
bennimoser <62222316+bennimoser@users.noreply.github.com>
2021-02-23 13:04:26 +0100

    Push Aufgabe 11

---
+++ Aufgaben/Aufgabe11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "D:/Repos/OOP_Aufgaben/Aufgaben/Aufgabe3/bin/debug/Aufgabe3.exe",
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += Process_OutputDataReceived;
+
+            process.Start();
+            process.BeginOutputReadLine();
+
+            Console.ReadKey();
+        }
+
+        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+    }
+}
