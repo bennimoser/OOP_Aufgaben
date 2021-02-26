db42106506ab0b3536744ea1540ea7bce1cb0bcf
bennimoser <62222316+bennimoser@users.noreply.github.com>
2021-02-26 16:25:40 +0100

    Push TPL Example

---
+++ Aufgaben/TPLExample/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPLExample
+{
+    class Program
+    {
+        public delegate int BinaryOperation(int x, int y);
+
+        public static void Main(string[] args)
+        {
+            BinaryOperation operation = new BinaryOperation(Add);
+            Console.WriteLine("Method was called on Thread " + Thread.CurrentThread.ManagedThreadId);
+            IAsyncResult asyncresult = operation.BeginInvoke(3, 4, new AsyncCallback(ExtractResult), new int[] { 3, 4 });
+            while (!asyncresult.IsCompleted)
+            {
+                Console.WriteLine("Still working on the Operation");
+                Thread.Sleep(1000);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("That was it with Delegates and IAsyncCallback");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Timers");
+
+
+
+
+            Timer timer = new Timer(new TimerCallback(TimerCallBack), "Hallihallo! Es ist : ", new TimeSpan(0, 0, 0), new TimeSpan(0, 0, 1));
+            Thread.Sleep(10000);
+            timer.Dispose();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("That was it with timers");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Parallel");
+
+            Parallel.For(0, 10, i =>
+              {
+                  Console.WriteLine("Iteration " + i);
+                  Thread.Sleep(1000);
+              });
+
+            Console.WriteLine();
+            Console.WriteLine("That was it with Parallel");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Tasks");
+
+            Task myTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Do something in the task, it is" + DateTime.Now.ToLongTimeString());
+            });
+            Console.WriteLine("Do something in the task, it is" + DateTime.Now.ToLongTimeString());
+
+            myTask.Wait();
+        }
+
+        public static int Add(int x, int y)
+        {
+            Console.WriteLine("Method was executed on Thread " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(10000);
+            return x + y;
+        }
+
+        public static void ExtractResult(IAsyncResult asresult)
+        {
+            AsyncResult ar = (AsyncResult)asresult;
+            BinaryOperation binary = (BinaryOperation)ar.AsyncDelegate;
+            int result = binary.EndInvoke(ar);
+            int[] array = (int[])ar.AsyncState;
+            Console.WriteLine("Its finished! The Result of " + array[0] + " + " + array[1] + ": " + result);
+        }
+
+        public static void TimerCallBack(object state)
+        {
+            Console.WriteLine("Method was called on Thread Nr. " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine((string)state + DateTime.Now.ToLongTimeString());
+        }
+    }
+
+}
