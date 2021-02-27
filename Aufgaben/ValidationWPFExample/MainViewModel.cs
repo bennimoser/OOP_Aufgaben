2f78496732157765cdb1d552e64a52b701487416
bennimoser <62222316+bennimoser@users.noreply.github.com>
2021-02-27 16:04:45 +0100

    Push WPF Validation Example

---
+++ Aufgaben/ValidationWPFExample/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationWPFExample
+{
+    public class MainViewModel
+    {
+        private string textBoxContent;
+        public string TextBoxContent
+        {
+            get
+            {
+                return textBoxContent;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.ToUpper() == "HUND")
+                {
+                    textBoxContent = value;
+                }
+                else
+                {
+                    throw new Exception("Das ist kein Hund!!!!");
+                }
+            }
+        }
+    }
+}
