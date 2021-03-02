722edb832d60710adcbbd5e6a6d0e632c326debd
bennimoser <62222316+bennimoser@users.noreply.github.com>
2021-03-02 17:34:25 +0100

    System.IO Exercises

---
+++ Aufgaben/SerialiseSystemIOExercise/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerialiseSystemIOExercise
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            #region XML Serializing
+            Person person1 = new Person() { Firstname = "Benjamin", Lastname = "Moser", Birthday = new BirthDate() { Month = 11, Day = 11, Year = 2000 }, _sex = "Tomate" };
+            Console.WriteLine("Before Serialize");
+            person1.WriteInformation();
+            
+            XmlSerializer xml = new XmlSerializer(typeof(Person));
+
+            FileStream stream = new FileStream("serialize.txt", FileMode.Create);
+
+            xml.Serialize(stream, person1);
+            stream.Close();
+            Console.WriteLine("After Serialize");
+            stream = new FileStream("serialize.txt", FileMode.Open);
+
+            var personresult = (Person)xml.Deserialize(stream);
+            personresult.WriteInformation();
+            stream.Close();
+
+            #endregion
+
+            Console.WriteLine();
+
+            #region Own Formatter
+
+            MyFormatter formatter = new MyFormatter();
+            stream = new FileStream("serializebinary.txt", FileMode.Create);
+            formatter.Serialize(stream, person1);
+
+            stream.Close();
+            stream = new FileStream("serializebinary.txt", FileMode.Open);
+            var binaryPerson = (Person)formatter.Deserialize(stream);
+
+            #endregion
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public BirthDate Birthday { get; set; }
+
+        [XmlIgnore]
+        [NonSerialized]
+        public string _sex;
+
+
+        public void WriteInformation()
+        {
+            Console.WriteLine("Firstname: " + Firstname);
+            Console.WriteLine("Lastname : " + Lastname);
+            Console.WriteLine("Day of Birth: {0}.{1}.{2}", Birthday.Day, Birthday.Month, Birthday.Year);
+        }
+    }
+
+    [Serializable]
+    public class BirthDate
+    {
+        public int Day { get; set; }
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+    }
+
+    public class MyFormatter : IFormatter
+    {
+        public ISurrogateSelector SurrogateSelector { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public SerializationBinder Binder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public StreamingContext Context { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public object Deserialize(Stream serializationStream)
+        {
+            Console.WriteLine("Formatter is now Deserializing");
+            BinaryFormatter formatter = new BinaryFormatter();
+            return formatter.Deserialize(serializationStream);
+        }
+
+        public void Serialize(Stream serializationStream, object graph)
+        {
+            Console.WriteLine("Formatter is now Serializing");
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(serializationStream, graph);
+        }
+    }
+}
---
+++ Aufgaben/SystemIOExercise/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIOExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region DriveInformation
+
+            DriveInfo[] driveinfos = DriveInfo.GetDrives();
+            foreach(DriveInfo driveinfo in driveinfos)
+            {
+                Console.WriteLine(driveinfo.Name);
+                Console.WriteLine(driveinfo.TotalFreeSpace / 1024 * 1024);
+                Console.WriteLine(driveinfo.DriveFormat);
+                Console.WriteLine(driveinfo.TotalSize / 1024 * 1024);
+                Console.WriteLine(driveinfo.DriveType.ToString());
+                Console.WriteLine("-----------------");
+            }
+
+            #endregion
+            #region Path, Directory, File
+
+            if (!Directory.Exists("C:/Temp"))
+            {
+                Directory.CreateDirectory("C:/Temp");
+            }
+
+            if (!File.Exists("C:/Temp/test.txt"))
+            {
+                File.Create("C:/Temp/test.txt");
+            }
+
+            List<string> files = Directory.GetFiles("C:/Temp", "*.txt", SearchOption.AllDirectories).ToList();
+
+            foreach(var file in files)
+            {
+                var fullpath = Path.GetFileNameWithoutExtension(file);
+            }
+
+            #endregion
+
+        }
+    }
+}
